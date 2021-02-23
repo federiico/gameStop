a4def8f5a2f94e07142ea88f2b9b8175a22c38ff
Davide <davide.palombaro@yahoo.com>
2021-02-23 12:26:48 +0100

    modifica homepage

---
+++ include/dbms.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+	$mysqli = new mysqli("localhost", "root", "", "gamestop");
+
+	if ($mysqli->connect_errno) {
+		printf("Connect failed: %s\n", $mysqli->connect_error);
+		exit();
+	}
+
+?>
