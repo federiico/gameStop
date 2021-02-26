5101c72dd96e00f5733f33b0d4282ee7a47bfa9e
Davide <davide.palombaro@yahoo.com>
2021-02-26 19:03:31 +0100

    inizio search

--- component/shop-page.comp.php
+++ component/shop-page.comp.php
@@ -4,7 +4,8 @@
     
         $console=$_GET['console'];
         $categoria=$_GET['categoria'];
-        $ordine = $_POST['orderby'];
+        $ordine = $_GET['orderby'];
+        $nome = $_GET['ricerca'];
 
         if($console== "PlayStation4" ) $console="Playstation 4";
         if($console== "XboxOne" ) $console="Xbox One";
@@ -93,5 +94,30 @@
                 }		
         }
 
-        //if(!(mysql_num_fields($result))) $body->setContent("NomeCategoria","Nessun Prodotto Rispetta i termini della tua ricerca");
+        //Shop By name
+        if((isset($nome))){
+            $query = "SELECT Id_articolo,Nome,Prezzo,Sconto,Descrizione FROM Articolo WHERE Nome LIKE '%".$nome."%' ORDER BY '".$ordine."' ".$verso."";
+            $result = $mysqli -> query($query);
+                while($gioco = $result -> fetch_assoc()){
+
+                    $body -> setContent("Immagine","getImage.php?Id_articolo=".$gioco['Id_articolo']);
+                    $body -> setContent("Nome_articolo",$gioco['Nome']);
+                    $body -> setContent("Descrizione_articolo",$gioco['Descrizione']);
+
+                    if($gioco['Sconto'] > 0){
+
+                        $body -> setContent("Offerta","Offerta");
+                        $nuovoPrezzo = ($giocoPS4['Prezzo'] - (($gioco['Prezzo'] / 100) * $gioco['Sconto'])); //prezzo scontato
+                        $nuovoPrezzo = number_format((float)$nuovoPrezzo, 2, '.', ''); //approssimo a 2 cifre decimali
+                        $body -> setContent("Vecchio_prezzo","€ ".$gioco['Prezzo']);
+                        $body -> setContent("Prezzo_corrente","€ ".$nuovoPrezzo);
+                    }
+                    else $body -> setContent("Prezzo_corrente","€ ".$gioco['Prezzo']);
+
+                    $body -> setContent("Link_prodotto","dettagli-prodotto.php?Id_articolo=".$gioco['Id_articolo']);
+                    $body -> setContent("conta", $query['conta']);
+
+                }
+            $body -> setContent("conta", $conta['conta']);
+        }
 ?>
