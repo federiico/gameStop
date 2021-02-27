58690e0c569dc718bf5941c7a69c93f4357b2916
Davide <davide.palombaro@yahoo.com>
2021-02-27 11:46:29 +0100

    merge

--- component/shop-page.comp.php
+++ component/shop-page.comp.php
@@ -17,9 +17,33 @@
 
         if($categoria=="GiochidiGuida") $categoria="Giochi di guida";
 
-        if(!(isset($console) && isset($categoria))){
+        if((isset($console) && isset($categoria))){
+            //Shop by Console+Category
+            $body->setContent("NomeCategoria",$verso);
+            $query ="SELECT Id_articolo,Nome,Prezzo,Sconto,Descrizione FROM Articolo WHERE Genere = '".$categoria."' AND Piattaforma = '".$console."' ORDER BY '".$ordine."' ".$verso."";
+            $result = $mysqli -> query($query);
+        
+                while($gioco = $result -> fetch_assoc()){
+
+                    $body -> setContent("Immagine","getImage.php?Id_articolo=".$gioco['Id_articolo']);
+                    $body -> setContent("Nome_articolo",$gioco['Nome']);
+                    $body -> setContent("Descrizione_articolo",$gioco['Descrizione']);
+
+                    if($gioco['Sconto'] > 0){
+
+                        $body -> setContent("Offerta","Offerta");
+                        $nuovoPrezzo = ($giocoPS4['Prezzo'] - (($gioco['Prezzo'] / 100) * $gioco['Sconto'])); //prezzo scontato
+                        $nuovoPrezzo = number_format((float)$nuovoPrezzo, 2, '.', ''); //approssimo a 2 cifre decimali
+                        $body -> setContent("Vecchio_prezzo","€ ".$gioco['Prezzo']);
+                        $body -> setContent("Prezzo_corrente","€ ".$nuovoPrezzo);
+                    }
+                    else $body -> setContent("Prezzo_corrente","€ ".$gioco['Prezzo']);
+
+                    $body -> setContent("Link_prodotto","dettagli-prodotto.php?Id_articolo=".$gioco['Id_articolo']);
+                }		
+        }else{
             //Shop by Console
-            if((isset($console))){
+            if((isset($console)) && is_null($nome)){
                 $query = "SELECT Id_articolo,Nome,Prezzo,Sconto,Descrizione FROM Articolo WHERE Piattaforma = '".$console."' ORDER BY '".$ordine."' ".$verso."";
                 $result = $mysqli -> query($query);
                     while($gioco = $result -> fetch_assoc()){
@@ -68,35 +92,11 @@
                         $body -> setContent("Link_prodotto","dettagli-prodotto.php?Id_articolo=".$gioco['Id_articolo']);
                     }		
             }
-        }else{
-            //Shop by Console+Category
-            $body->setContent("NomeCategoria",$verso);
-            $query ="SELECT Id_articolo,Nome,Prezzo,Sconto,Descrizione FROM Articolo WHERE Genere = '".$categoria."' AND Piattaforma = '".$console."' ORDER BY '".$ordine."' ".$verso."";
-            $result = $mysqli -> query($query);
-        
-                while($gioco = $result -> fetch_assoc()){
-
-                    $body -> setContent("Immagine","getImage.php?Id_articolo=".$gioco['Id_articolo']);
-                    $body -> setContent("Nome_articolo",$gioco['Nome']);
-                    $body -> setContent("Descrizione_articolo",$gioco['Descrizione']);
-
-                    if($gioco['Sconto'] > 0){
-
-                        $body -> setContent("Offerta","Offerta");
-                        $nuovoPrezzo = ($giocoPS4['Prezzo'] - (($gioco['Prezzo'] / 100) * $gioco['Sconto'])); //prezzo scontato
-                        $nuovoPrezzo = number_format((float)$nuovoPrezzo, 2, '.', ''); //approssimo a 2 cifre decimali
-                        $body -> setContent("Vecchio_prezzo","€ ".$gioco['Prezzo']);
-                        $body -> setContent("Prezzo_corrente","€ ".$nuovoPrezzo);
-                    }
-                    else $body -> setContent("Prezzo_corrente","€ ".$gioco['Prezzo']);
-
-                    $body -> setContent("Link_prodotto","dettagli-prodotto.php?Id_articolo=".$gioco['Id_articolo']);
-                }		
         }
-
-        //Shop By name
-        if((isset($nome))){
-            $query = "SELECT Id_articolo,Nome,Prezzo,Sconto,Descrizione FROM Articolo WHERE Nome LIKE '%".$nome."%' ORDER BY '".$ordine."' ".$verso."";
+        //Shop By name + console
+        
+        if((isset($nome)) && isset($console)){
+            $query = "SELECT Id_articolo,Nome,Prezzo,Sconto,Descrizione FROM Articolo WHERE (Nome LIKE '%".$nome."%' OR Produttore LIKE '%".$nome."%') AND Piattaforma = '".$console."' ORDER BY '".$ordine."' ".$verso."";
             $result = $mysqli -> query($query);
                 while($gioco = $result -> fetch_assoc()){
 
@@ -120,4 +120,34 @@
                 }
             $body -> setContent("conta", $conta['conta']);
         }
+
+
+        //Shop By name
+
+        if((isset($nome)) && $console=="1" ){
+            $query = "SELECT Id_articolo,Nome,Prezzo,Sconto,Descrizione FROM Articolo WHERE Nome LIKE '%".$nome."%' OR Produttore LIKE '%".$nome."%' ORDER BY '".$ordine."' ".$verso."";
+            $result = $mysqli -> query($query);
+                while($gioco = $result -> fetch_assoc()){
+    
+                    $body -> setContent("Immagine","getImage.php?Id_articolo=".$gioco['Id_articolo']);
+                    $body -> setContent("Nome_articolo",$gioco['Nome']);
+                    $body -> setContent("Descrizione_articolo",$gioco['Descrizione']);
+        
+                    if($gioco['Sconto'] > 0){
+        
+                    $body -> setContent("Offerta","Offerta");
+                    $nuovoPrezzo = ($giocoPS4['Prezzo'] - (($gioco['Prezzo'] / 100) * $gioco['Sconto'])); //prezzo scontato
+                    $nuovoPrezzo = number_format((float)$nuovoPrezzo, 2, '.', ''); //approssimo a 2 cifre decimali
+                    $body -> setContent("Vecchio_prezzo","€ ".$gioco['Prezzo']);
+                    $body -> setContent("Prezzo_corrente","€ ".$nuovoPrezzo);
+                    }
+                    else $body -> setContent("Prezzo_corrente","€ ".$gioco['Prezzo']);
+        
+                    $body -> setContent("Link_prodotto","dettagli-prodotto.php?Id_articolo=".$gioco['Id_articolo']);
+                    $body -> setContent("conta", $query['conta']);
+        
+                    }
+                    $body -> setContent("conta", $conta['conta']);
+                }
+
 ?>
